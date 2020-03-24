f27ad3a0f31eb5781405e00c61536aacea980f3a
Alistair Gempf <aligempf@gmail.com>
2020-03-24 20:04:27 +0000

    Test init

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+const Home = () => (
+    <div style={{backgroundImage: 'Url("/bar.png")', height: '100%', width: '100%'}}>
+        <img style={{maxWidth: '100vh', maxHeight: '100vw'}} src="/dishyrishilogo.png" />
+    </div>
+);
+
+export default Home;
