dc031cd5e17f9c07f68b61c1ad88e8871625c559
Alistair Gempf <aligempf@gmail.com>
2020-04-02 13:01:52 +0100

    Use styled components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
+import styledNormalize from 'styled-normalize';
+import { createGlobalStyle } from 'styled-components';
+
+const boxSizing = `/* Box Sizing https://bit.ly/1A91I0J */
+    html {
+      box-sizing: border-box;
+      font-size: 100%;
+    }
+    *, *:before, *:after {
+      box-sizing: inherit;
+    }`;
+
+const GlobalStyles = createGlobalStyle`
+    ${styledNormalize}
+    
+    ${boxSizing}
+
+    html{
+      ${({ fonts }) => fonts || null}
+    }
+  `;
+
 const Home = () => (
-    <div style={{backgroundImage: 'Url("/bar.png")', height: '100%', width: '100%'}}>
-        <img style={{maxWidth: '100vh', maxHeight: '100vw'}} src="/dishyrishilogo.png" />
-    </div>
+    <>
+        <GlobalStyles />
+        <div style={{backgroundImage: 'Url("/bar.png")', height: '100%', width: '100%', margin:0, padding:0}}>
+            <img style={{maxWidth: '100vh', maxHeight: '100vw'}} src="/dishyrishilogo.png" />
+        </div>
+    </>
 );
 
 export default Home;
